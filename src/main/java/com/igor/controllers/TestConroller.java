981b76d9d4f4d445ff59cbca39329acc2e845cef
kenny1314 <GGigrik77>
2019-12-17 03:20:58 +0300

    index.html create

---
+++ src/main/java/com/igor/controllers/TestConroller.java
@@ -0,0 +1,20 @@
+package com.igor.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class TestConroller {
+
+    private static Logger logger = LoggerFactory.getLogger(TestConroller.class);
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+}
--- src/main/java/com/igor/entity/Cafe.java
+++ src/main/java/com/igor/entity/Cafe.java
@@ -1,11 +1,7 @@
 package com.igor.entity;
 
-import org.graalvm.compiler.nodeinfo.StructuralInput;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Cafe extends AbstractEntity{
